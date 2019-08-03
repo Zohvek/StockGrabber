ef9e6d88b546cdc14188cffcd6bd53b0e16bde1e
Ryan Hutton <hatnice42@gmail.com>
2019-08-02 22:23:48 -0500

    Add project files.

---
+++ APIGateWay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockGrabber
+{
+    class APIGateWay
+    {
+        const string urlBase = "https://www.alphavantage.co/";
+        private const string APIKey = "--";
+        public string keywords;
+        public string APIFunction;
+
+    }
+}
